f5705df2370c4d6de733f0164e20a27a54a0cd5c
jivvan <jivan077@academiacollege.edu.np>
2025-01-02 20:11:06 +0545

    model complete

--- frontend/src/Components/Charts/SavingGoals.jsx
+++ frontend/src/Components/Charts/SavingGoals.jsx
@@ -6,32 +6,33 @@ const SavingGoals = () => {
   const goals = useStore((state) => state.goals);
 
   return (
-    <Card className="p-4">
-      <h2 className="mb-4 text-lg font-semibold">Saving Goals</h2>
-      {goals.length > 0 ? (
-        <>
-          {goals.map((goal, index) => (
-            <div key={index} className="mb-4">
-              <div className="flex justify-between mb-2">
-                <span>{goal.name}</span>
-                <span>
-                  ${goal.current.toFixed(2)} / ${goal.target.toFixed(2)}
-                </span>
-              </div>
-              <Progress
-                progress={((goal.current / goal.target) * 100).toFixed(2)}
-                size="lg"
-                color={getColorPerProgress((goal.current / goal.target) * 100)}
-                labelProgress
-                progressLabelPosition="outside"
-              />
-            </div>
-          ))}
-        </>
-      ) : (
-        <p>Create saving goals to see them here</p>
-      )}
-    </Card>
+    // <Card className="p-4">
+    //   <h2 className="mb-4 text-lg font-semibold">Saving Goals</h2>
+    //   {goals.length > 0 ? (
+    //     <>
+    //       {goals.map((goal, index) => (
+    //         <div key={index} className="mb-4">
+    //           <div className="flex justify-between mb-2">
+    //             <span>{goal.name}</span>
+    //             <span>
+    //               ${goal.current.toFixed(2)} / ${goal.target.toFixed(2)}
+    //             </span>
+    //           </div>
+    //           <Progress
+    //             progress={((goal.current / goal.target) * 100).toFixed(2)}
+    //             size="lg"
+    //             color={getColorPerProgress((goal.current / goal.target) * 100)}
+    //             labelProgress
+    //             progressLabelPosition="outside"
+    //           />
+    //         </div>
+    //       ))}
+    //     </>
+    //   ) : (
+    //     <p>Create saving goals to see them here</p>
+    //   )}
+    // </Card>
+    <h1>WIP</h1>
   );
 };
 
